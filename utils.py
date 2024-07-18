387cb910384b1a9e15648f39c44a8ebd42843d6c
Dmitriy Sofronov <profos24@gmail.com>
2024-07-18 19:50:08 +0400

    docs: Add docstrings to utility functions for improved clarity

--- utils.py
+++ utils.py
@@ -4,6 +4,18 @@ from enums import BookStatus
 
 
 def get_int_input(prompt: str) -> int:
+    """
+    Запрашивает у пользователя ввод натурального числа.
+
+    Args:
+        prompt (str): Приглашение к вводу, отображаемое пользователю.
+
+    Returns:
+        int: Введенное пользователем натуральное число.
+
+    Raises:
+        ValueError: Если введено отрицательное число или нечисловое значение.
+    """
     while True:
         try:
             numb = int(input(prompt))
@@ -15,6 +27,18 @@ def get_int_input(prompt: str) -> int:
 
 
 def get_year_input(prompt: str) -> int:
+    """
+    Запрашивает у пользователя ввод года, не превышающего текущий.
+
+    Args:
+        prompt (str): Приглашение к вводу, отображаемое пользователю.
+
+    Returns:
+        int: Введенный пользователем год.
+
+    Raises:
+        ValueError: Если введен год, превышающий текущий, или нечисловое значение.
+    """
     while True:
         try:
             year = get_int_input(prompt)
@@ -26,6 +50,15 @@ def get_year_input(prompt: str) -> int:
 
 
 def get_book_status() -> BookStatus:
+    """
+    Запрашивает у пользователя ввод статуса книги.
+
+    Returns:
+        BookStatus: Введенный пользователем статус книги (AVAILABLE или BORROWED).
+
+    Raises:
+        ValueError: Если введен некорректный статус.
+    """
     while True:
         status = input(f"Введите новый статус ({BookStatus.AVAILABLE.value}/{BookStatus.BORROWED.value}): ").lower()
         if status == BookStatus.AVAILABLE.value:
@@ -36,6 +69,15 @@ def get_book_status() -> BookStatus:
 
 
 def print_search_results(results):
+    """
+    Выводит результаты поиска книг.
+
+    Args:
+        results (List[Book]): Список найденных книг.
+
+    Note:
+        Если список пуст, выводит сообщение "Книги не найдены."
+    """
     if results:
         for book in results:
             print(book)
