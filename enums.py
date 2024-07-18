c6b5a3f774124211b32fabf4832bb91b7e86e005
Dmitriy Sofronov <profos24@gmail.com>
2024-07-18 19:40:27 +0400

    feat: Implement MenuChoice enum

--- enums.py
+++ enums.py
@@ -4,3 +4,10 @@ class BookStatus(Enum):
     AVAILABLE = "в наличии"
     BORROWED = "выдана"
 
+class MenuChoice(Enum):
+    ADD_BOOK = '1'
+    REMOVE_BOOK = '2'
+    SEARCH_BOOK = '3'
+    LIST_BOOKS = '4'
+    UPDATE_STATUS = '5'
+    EXIT = '6'
