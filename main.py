20f9629a872b0a909f82a2a13a1cb4bf6a31278a
Dmitriy Sofronov <profos24@gmail.com>
2024-07-18 19:47:19 +0400

    refactor: Enhanced Library Management System with Improved Structure and Additional Features

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
+from typing import Callable
+
+from enums import MenuChoice
 from library import Library
+from utils import get_int_input, get_book_status, print_search_results, get_year_input
 
 
 def main():
-    library = Library()
+    """
+    Главная функция программы.
+
+    Инициализирует библиотеку, создает меню действий и
+    запускает основной цикл взаимодействия с пользователем.
+    """
+    library = Library('data.json')
+
+    menu_actions: dict[MenuChoice, Callable[[], None]] = {
+        MenuChoice.ADD_BOOK: lambda: library.add_book(
+            input("Введите название книги: "),
+            input("Введите автора книги: "),
+            get_year_input("Введите год издания книги: ")
+        ),
+        MenuChoice.REMOVE_BOOK: lambda: library.remove_book(
+            get_int_input("Введите ID книги для удаления: ")
+        ),
+        MenuChoice.SEARCH_BOOK: lambda: print_search_results(
+            library.search_books(input("Введите ключевое слово для поиска: "))
+        ),
+        MenuChoice.LIST_BOOKS: library.list_books,
+        MenuChoice.UPDATE_STATUS: lambda: library.update_book_status(
+            get_int_input("Введите ID книги для обновления статуса: "),
+            get_book_status()
+        ),
+        MenuChoice.EXIT: lambda: None
+    }
 
     while True:
         print("\nСистема управления библиотекой")
-        print("1. Добавить книгу")
-        print("2. Список книг")
-        print("3. Выход")
+        for choice in MenuChoice:
+            print(f"{choice.value}. {choice.name.replace('_', ' ').capitalize()}")
 
         choice = input("Выберите действие: ")
 
-        if choice == "1":
-            title = input("Введите название книги: ")
-            author = input("Введите автора книги: ")
-            year = int(input("Введите год издания книги: "))
-            library.add_book(title, author, year)
-        elif choice == "2":
-            library.list_books()
-        elif choice == "3":
-            break
-        else:
+        try:
+            action = menu_actions[MenuChoice(choice)]
+            if choice == MenuChoice.EXIT.value:
+                break
+            action()
+        except KeyError:
             print("Неверный выбор. Попробуйте снова.")
+
+
 
 
 if __name__ == "__main__":
