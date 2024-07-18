b5c55b89f8aea4c39cec9cde74e1aec00edd8fac
Dmitriy Sofronov <profos24@gmail.com>
2024-07-18 19:41:52 +0400

    feat: Implement additional utility functions

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from enums import BookStatus
+
 
 def get_int_input(prompt: str) -> int:
     while True:
@@ -21,3 +23,21 @@ def get_year_input(prompt: str) -> int:
             return year
         except ValueError:
             print("Ошибка: Пожалуйста, введите корректный год.")
+
+
+def get_book_status() -> BookStatus:
+    while True:
+        status = input(f"Введите новый статус ({BookStatus.AVAILABLE.value}/{BookStatus.BORROWED.value}): ").lower()
+        if status == BookStatus.AVAILABLE.value:
+            return BookStatus.AVAILABLE
+        elif status == BookStatus.BORROWED.value:
+            return BookStatus.BORROWED
+        print("Ошибка: Неверный статус. Попробуйте снова.")
+
+
+def print_search_results(results):
+    if results:
+        for book in results:
+            print(book)
+    else:
+        print("Книги не найдены.")
