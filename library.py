6bc45d4a7cb1ed6cce7378e5471cd8e02c04ac61
Dmitriy Sofronov <profos24@gmail.com>
2024-07-18 19:32:53 +0400

    feat: Implement book management functions

--- library.py
+++ library.py
@@ -17,3 +17,15 @@ class Library:
         else:
             for book in self.books:
                 print(book)
+
+    def remove_book(self, book_id: int) -> None:
+        for book in self.books:
+            if book.id == book_id:
+                self.books.remove(book)
+                print(f"Книга с ID {book_id} удалена.")
+                return
+        print(f"Книга с ID {book_id} не найдена.")
+
+    def search_books(self, keyword: str) -> list[Book]:
+        return [book for book in self.books if keyword.lower() in book.title.lower() or
+                keyword.lower() in book.author.lower() or keyword == str(book.year)]
